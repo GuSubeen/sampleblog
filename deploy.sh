8b4ee1ea6a5baea2a5fcdaca91014f656a738fea
구수빈 <rntnqls9@naver.com>
2022-04-07 13:58:11 +0900

    --all.sh main

--- deploy.sh
+++ deploy.sh
@@ -4,20 +4,20 @@
 set -e
 
 # pull, commit, push
-#git pull
-#git add -A
-#git commit -m "$1 $2 $3 --all.sh main"
-#git push origin main
+git pull
+git add -A
+git commit -m "$1 $2 $3 --all.sh main"
+git push origin main
 
 # 문서를 build하여 html로 만든다
-#yarn build
+yarn build
 
 # build가 output된 폴더로 이동한다.
-# cd .publi
+cd ../../public
 
 # git init, commit, push
-#git init
-git add .
+git init
+git add -A
 git commit -m 'deploy with vuepress'
 
 git push -f https://github.com/GuSubeen/blog.git main:gh-pages

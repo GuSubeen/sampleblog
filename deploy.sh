f9fc9900bb612f79cd34993932b5d12836014863
구수빈 <rntnqls9@naver.com>
2022-04-07 13:48:24 +0900

    --all.sh main

--- deploy.sh
+++ deploy.sh
@@ -13,14 +13,14 @@ git push origin main
 yarn build
 
 # build가 output된 폴더로 이동한다.
-cd ./public
+cd ../public
 
 # git init, commit, push
 git init
 git add -A
 git commit -m 'deploy with vuepress'
 
-git push -f https://github.com/GuSubeen/blog main:gh-pages
+git push -f https://github.com/GuSubeen/blog.git main:gh-pages
 
 cd -
 

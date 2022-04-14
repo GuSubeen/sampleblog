04d0c4c52fe71115a28b30c930a6c1fefdd6c2db
구수빈 <rntnqls9@naver.com>
2022-04-14 15:49:47 +0900

    reco 모듈 추가

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,7 +16,7 @@ module.exports = {
     ['link', { rel: 'icon', href: 'images/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
   ],
-  theme: 'reco',
+  //theme: path.resolve(__dirname,'./theme/vuepress-theme-reco'),
   themeConfig: {
     nav: [
       { text: 'Home', link: '/', icon: 'reco-home' },
